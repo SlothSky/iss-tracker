a1a2ee57349292601c1e780be62e32091cace816
SlotH <jan.weiss5@gmail.com>
2021-08-26 19:12:19 +0200

    connect coordinate and iss modules

--- src/iss/iss.controller.ts
+++ src/iss/iss.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { CoordinatesService } from 'src/coordinates/coordinates.service';
 import { ISSService } from './iss.service';
 
 @Controller()
 export class ISSController {
-  constructor(private readonly issService: ISSService) {}
+  constructor(
+    private readonly issService: ISSService,
+    private coordinatesService: CoordinatesService,
+  ) {}
 
   @Get('test')
   specificRedirect(): string {
+    this.coordinatesService.insertISStoMap('');
+
     return this.issService.getIssMap('');
   }
 }
--- src/iss/iss.module.ts
+++ src/iss/iss.module.ts
@@ -1,10 +1,12 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AgentControlMiddleware } from 'src/middleware/agent-control.middleware';
+
+import { CoordinatesModule } from 'src/coordinates/coordinates.module';
 import { ISSController } from './iss.controller';
 import { ISSService } from './iss.service';
 
 @Module({
-  imports: [],
+  imports: [CoordinatesModule],
   controllers: [ISSController],
   providers: [ISSService],
 })
