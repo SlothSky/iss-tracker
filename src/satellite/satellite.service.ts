375619559618a9af323852abce1b9321ab53e8f6
AKKA\Jan.WEISS <jan.weiss@akka.eu>
2021-09-08 13:54:07 +0200

    add user agent specific color characters to map creation

--- src/satellite/satellite.service.ts
+++ src/satellite/satellite.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 
 @Injectable()
 export class SatelliteService {
+  private TERMINAL_COLOR_PREFIX = '\u001b[31m';
+  private TERMINAL_COLOR_SUFFIX = '\u001b[0m';
+
+  private BROWSER_COLOR_PREFIX = '<span>';
+  private BROWSER_COLOR_SUFFIX = '</span>';
+  
   /**
    * The _getSatelliteMap()_ is the main coordination function for getting the text version of a map.
    * This function also calls the required functions for inserting the requested sattelite's position.
@@ -13,7 +19,7 @@ export class SatelliteService {
   getSatelliteMap(usedTech: string, indeces: Array<number>): string {
     let map: string = fs.readFileSync('./src/assets/map.txt', 'utf8');
 
-    map = this.addCoordinatesToMap(map, indeces);
+    map = this.addCoordinatesToMap(usedTech, map, indeces);
 
     return map;
   }
@@ -26,16 +32,32 @@ export class SatelliteService {
    * @param index The index for the character, which shall be replaced.
    * @returns The mapInput ASCII map with the ISS dot inserted.
    */
-  addCoordinatesToMap(mapInput: string, issIndeces: Array<number>) {
-    // first replace upper neighbour w/ white space, then ISS with red dot, then lower neighbour w/ white space
-    let mapOutput =
-      mapInput.substring(0, issIndeces[1] - 1) +
-      ' \u001b[31m#\u001b[0m ' +
-      mapInput.substring(issIndeces[1] + 2, issIndeces[2] - 1) + 
-      ' \u001b[31m█\u001b[0m ' +
-      mapInput.substring(issIndeces[2] + 2, issIndeces[0] - 1) +
-      ' \u001b[31m#\u001b[0m ' +
-      mapInput.substring(issIndeces[0] + 2);
+  addCoordinatesToMap(usedTech: string, mapInput: string, issIndeces: Array<number>) {
+    // depending on user agent first replace upper neighbour w/ white space, then ISS with red dot, then lower neighbour w/ white space
+    if(usedTech === 'terminal')
+    {    
+      var mapOutput =
+        mapInput.substring(0, issIndeces[1] - 1) +
+        ' ' + this.TERMINAL_COLOR_PREFIX + '#' + this.TERMINAL_COLOR_SUFFIX + ' ' +
+        mapInput.substring(issIndeces[1] + 2, issIndeces[2] - 1) + 
+        ' ' + this.TERMINAL_COLOR_PREFIX + '█' + this.TERMINAL_COLOR_SUFFIX + ' ' +
+        mapInput.substring(issIndeces[2] + 2, issIndeces[0] - 1) +
+        ' ' + this.TERMINAL_COLOR_PREFIX + '#' + this.TERMINAL_COLOR_SUFFIX + ' ' +
+        mapInput.substring(issIndeces[0] + 2);
+    }
+    else if(usedTech === 'browser') {
+      var mapOutput =
+        mapInput.substring(0, issIndeces[1] - 1) +
+        ' ' + this.BROWSER_COLOR_PREFIX + '#' + this.BROWSER_COLOR_SUFFIX + ' ' +
+        mapInput.substring(issIndeces[1] + 2, issIndeces[2] - 1) + 
+        ' ' + this.BROWSER_COLOR_PREFIX + '█' + this.BROWSER_COLOR_SUFFIX + ' ' +
+        mapInput.substring(issIndeces[2] + 2, issIndeces[0] - 1) +
+        ' ' + this.BROWSER_COLOR_PREFIX + '#' + this.BROWSER_COLOR_SUFFIX + ' ' +
+        mapInput.substring(issIndeces[0] + 2);
+    }
+    else {
+      throw new HttpException('This user agent is not known by the server', 500);
+    }
 
     return mapOutput;
   }
