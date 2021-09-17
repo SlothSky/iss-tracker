c6ce010c52c2f4bfd45ae496b2385339c1966970
Marc Rominger <Marcrominger@gmx.de>
2021-09-17 20:48:57 +0200

    fix promises

--- src/satellite/satellite.controller.ts
+++ src/satellite/satellite.controller.ts
@@ -1,40 +1,32 @@
-import {
-  Controller,
-  Get,
-  Headers
-} from '@nestjs/common';
-import { CoordinatesService } from '../coordinates/coordinates.service';
-import { SatelliteService } from './satellite.service';
-@Controller({ host: 'iss.bianco-solutions.de' })
+import {Controller, Get, Headers} from '@nestjs/common';
+import {CoordinatesService} from '../coordinates/coordinates.service';
+import {SatelliteService} from './satellite.service';
+
+@Controller({host: 'iss.bianco-solutions.de'})
 export class SatelliteController {
-  returnMap: string;
-  validator = false;
+    returnMap: string;
 
-  constructor(
-    private readonly satelliteService: SatelliteService,
-    private coordinatesService: CoordinatesService,
-  ) {}
-
-  @Get()
-  async getBrowserMap(
-    @Headers('user-agent') user_agent: string,
-  ): Promise<string> {
-    if (user_agent.includes('curl')) {
-      user_agent = 'terminal';
-    } else {
-      user_agent = 'browser';
+    constructor(
+        private readonly satelliteService: SatelliteService,
+        private coordinatesService: CoordinatesService,
+    ) {
     }
-    try {
-      await this.coordinatesService.coordinatesResolver(25544).then((data) => {
-        this.returnMap = this.satelliteService.getSatelliteMap(
-          user_agent,
-          data,
-        );
-      });
 
-      return this.returnMap;
-    } catch (error) {
-      return error;
+    @Get()
+    async getBrowserMap(
+        @Headers('user-agent') user_agent: string,
+    ): Promise<string> {
+        if (user_agent.includes('curl')) {
+            user_agent = 'terminal';
+        } else {
+            user_agent = 'browser';
+        }
+        try {
+            const data = await this.coordinatesService.coordinatesResolver(25544)
+            this.returnMap = this.satelliteService.getSatelliteMap(user_agent, data);
+            return this.returnMap;
+        } catch (error) {
+            return error;
+        }
     }
-  }
 }
